2fd3284b664c72ab7f876c018499991024f4df8f
Shivam Tiwari <shivamtiwari8302@gmail.com>
2026-04-21 15:21:29 +0530

    add exp1

---
+++ notebooks/dagshub_setup.py
@@ -0,0 +1,9 @@
+import mlflow
+
+import dagshub
+mlflow.set_tracking_uri('https://dagshub.com/shivamtiwari83032-collab/mini-mlops-proj.mlflow')
+dagshub.init(repo_owner='shivamtiwari83032-collab', repo_name='mini-mlops-proj', mlflow=True)
+
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
