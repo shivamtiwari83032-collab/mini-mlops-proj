a9b17e4ffbb065cafbdeb98bc57eb17b89a844ad
Shivam Tiwari <shivamtiwari8302@gmail.com>
2026-04-28 08:23:48 +0530

    now work

--- notebooks/dagshub_setup.py
+++ notebooks/dagshub_setup.py
@@ -7,3 +7,7 @@ dagshub.init(repo_owner='shivamtiwari83032-collab', repo_name='mini-mlops-proj',
 with mlflow.start_run():
   mlflow.log_param('parameter name', 'value')
   mlflow.log_metric('metric name', 1)
+
+
+
+  # 27ac23ebe84b70294deff5c3a18f9fa1f97a03d8
