f1b6303c9aca79baba257ce577b003704577fad6
Shivam Tiwari <shivamtiwari8302@gmail.com>
2026-04-28 23:14:03 +0530

    add model test 1

--- test/test_model.py
+++ test/test_model.py
@@ -47,47 +47,5 @@ class TestModelLoading(unittest.TestCase):
     def test_model_loaded_properly(self):
         self.assertIsNotNone(self.new_model)
 
-    def test_model_signature(self):
-        # Create a dummy input for the model based on expected input shape
-        input_text = "hi how are you"
-        input_data = self.vectorizer.transform([input_text])
-        input_df = pd.DataFrame(input_data.toarray(), columns=[str(i) for i in range(input_data.shape[1])])
-
-        # Predict using the new model to verify the input and output shapes
-        prediction = self.new_model.predict(input_df)
-
-        # Verify the input shape
-        self.assertEqual(input_df.shape[1], len(self.vectorizer.get_feature_names_out()))
-
-        # Verify the output shape (assuming binary classification with a single output)
-        self.assertEqual(len(prediction), input_df.shape[0])
-        self.assertEqual(len(prediction.shape), 1)  # Assuming a single output column for binary classification
-
-    def test_model_performance(self):
-        # Extract features and labels from holdout test data
-        X_holdout = self.holdout_data.iloc[:,0:-1]
-        y_holdout = self.holdout_data.iloc[:,-1]
-
-        # Predict using the new model
-        y_pred_new = self.new_model.predict(X_holdout)
-
-        # Calculate performance metrics for the new model
-        accuracy_new = accuracy_score(y_holdout, y_pred_new)
-        precision_new = precision_score(y_holdout, y_pred_new)
-        recall_new = recall_score(y_holdout, y_pred_new)
-        f1_new = f1_score(y_holdout, y_pred_new)
-
-        # Define expected thresholds for the performance metrics
-        expected_accuracy = 0.40
-        expected_precision = 0.40
-        expected_recall = 0.40
-        expected_f1 = 0.40
-
-        # Assert that the new model meets the performance thresholds
-        self.assertGreaterEqual(accuracy_new, expected_accuracy, f'Accuracy should be at least {expected_accuracy}')
-        self.assertGreaterEqual(precision_new, expected_precision, f'Precision should be at least {expected_precision}')
-        self.assertGreaterEqual(recall_new, expected_recall, f'Recall should be at least {expected_recall}')
-        self.assertGreaterEqual(f1_new, expected_f1, f'F1 score should be at least {expected_f1}')
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
